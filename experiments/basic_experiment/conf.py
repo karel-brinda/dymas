0e6df6452006f51d89a247a02a8b05d937042176
Karel Břinda <karel.brinda@gmail.com>
2015-09-23 17:49:21 +0200

    Basic experiment

--- experiments/basic_experiment/conf.py
+++ experiments/basic_experiment/conf.py
@@ -5,6 +5,9 @@ coverage=10
 
 def gp_style(i,nb):
 	assert i<nb
-	red=int(255.0*i/(nb-1))
+	if nb!=1:
+		red=int(255.0*i/(nb-1))
+	else:
+		red=0
 	color = "".join(["#","%0.2X" % red,"00","%0.2X" % (255-red)])
 	return 'set style line {i} lt 1 pt {i} lc rgb "{color}"'.format(i=i+1,color=color)
