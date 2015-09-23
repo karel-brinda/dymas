fd4cdcfb585fc54d67d1f564e96856c412cd57bf
Karel Břinda <karel.brinda@gmail.com>
2015-09-23 16:23:11 +0200

    Coverage parameter

--- dymas/dymas/Reads_Dyn.py
+++ dymas/dymas/Reads_Dyn.py
@@ -1,12 +1,24 @@
 from .Reads import Reads
+import math
 
 class Reads_Dyn(Reads):
 
-	def __init__(self,**kwds):
+	def __init__(self,
+				fastq_1_fn,
+				coverage,
+				coverage_per_iteration,
+				fastq_2_fn=None,
+			):
 
-		print(str(kwds))
-		super().__init__(**kwds)        
+		super().__init__(
+				fastq_1_fn=fastq_1_fn,
+				fastq_2_fn=fastq_2_fn,
+				coverage=coverage,
+			)
 
+		self.coverage_per_iteration=coverage_per_iteration
+		self.iterations=int(math.ceil(coverage/coverage_per_iteration))
+		self.reads_per_iteration=int(math.ceil((coverage_per_iteration/coverage)*self.reads))
 
 	def create_fastq_iteration(self,fastq_fn,iteration):
 		with open(self.fastq_1_fn) as inp:
--- dymas/dymas/Reads_ItRef.py
+++ dymas/dymas/Reads_ItRef.py
@@ -3,11 +3,22 @@ import shutil
 
 class Reads_ItRef(Reads):
 
-	def __init__(self,**kwds):
+	def __init__(self,
+				fastq_1_fn,
+				coverage,
+				iterations,
+				fastq_2_fn=None,
+			):
 
-		print(str(kwds))
-		super().__init__(**kwds)        
+		super().__init__(
+				fastq_1_fn=fastq_1_fn,
+				fastq_2_fn=fastq_2_fn,
+				coverage=coverage,
+			)
 
+		self.coverage_per_iteration=coverage
+		self.iterations=iterations
+		self.reads_per_iteration=self.reads
 
 	def create_fastq_iteration(self,fastq_fn,iteration):
 		shutil.copyfile(self.fastq_1_fn,fastq_fn)
