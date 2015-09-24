d2924f4e6170021a3dcfe01625bc69257fa74310
Karel Břinda <karel.brinda@gmail.com>
2015-09-25 01:42:39 +0200

    Other experiments

---
+++ experiments/exp.04__HumanMouse__0.15/run.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+set -e
+
+for snakefile in `ls Snakefile.*`; do
+	echo
+	echo "-------------"
+	echo "$snakefile"
+	echo "-------------"
+	echo
+	snakemake -s $snakefile --cores
+done
+
