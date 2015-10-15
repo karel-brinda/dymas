29a3dfd87bd21e7ba3151df557855df860235ca6
Karel Břinda <karel.brinda@gmail.com>
2015-10-15 19:27:12 +0200

    Human experiment

---
+++ experiments/exp.5.03__Human.chr21__0.07__consensusBcfTools/conf.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(0, "../")
+from human_conf import *
+
+conf_mutrate=0.07
+
+conf_description+="mutrate {}; BcfTools consensus;".format(conf_mutrate)
---
+++ experiments/human_conf.py
@@ -0,0 +1,11 @@
+from global_conf import *
+
+conf_reference="../exp.0.00__data/Human.chr21.fa"
+conf_coverage=10
+
+conf_contamination_reference="../exp.0.00__data/Mouse.chr19.fa"
+conf_contamination_coverage=5
+
+conf_title+="Human chr 21 (cont. Mouse chr 19 cov. 5)"
+conf_description+="cov 10; "
+conf_allowed_delta=5
