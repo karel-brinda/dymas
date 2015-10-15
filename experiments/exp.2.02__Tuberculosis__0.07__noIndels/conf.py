1778d02fc26bda2c614c521db31fca239bbd1f86
Karel Břinda <karel.brinda@gmail.com>
2015-10-15 19:03:52 +0200

    Experiment with Tuberculosis

---
+++ experiments/exp.2.02__Tuberculosis__0.07__noIndels/conf.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, "../")
+from tuberculosis_conf import *
+
+conf_mutrate=0.07
+conf_updates_indels=False
+
+conf_description+="mutrate {}; noindels;".format(conf_mutrate)
