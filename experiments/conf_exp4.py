18e3a37f5444aa8ffa87f0b1f975b56250ab8401
Karel Břinda <karel.brinda@gmail.com>
2016-03-23 23:04:36 +0100

    Add Exp4 Meningitidis

--- experiments/conf_exp4.py
+++ experiments/conf_exp4.py
@@ -1,11 +1,11 @@
 from conf_global import *
 
-conf_reference="../exp0.00__data/Mycobacterium_tuberculosis.fa"
+conf_reference="../exp0.00__data/Neisseria_meningitidis.fa"
 conf_coverage=10
 
 conf_contamination_reference="../exp0.00__data/Borrelia_garinii.fa"
 conf_contamination_coverage=5
 
-conf_title+="Mycobacterium tuberculosis (cont. Borrelia garinii)"
+conf_title+="Neisseria_meningitidis (cont. Borrelia)"
 #conf_description+="cov 5; "
 conf_allowed_delta=5
