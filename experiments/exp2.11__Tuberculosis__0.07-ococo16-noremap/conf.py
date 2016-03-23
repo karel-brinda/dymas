5ee0c9a2e45096951efe6e8895667ebc0e8e7000
Karel Břinda <karel.brinda@gmail.com>
2016-03-23 20:00:04 +0100

    Finish the previous corrections (bugs in remapping)

--- experiments/exp2.11__Tuberculosis__0.07-ococo16-noremap/conf.py
+++ experiments/exp2.11__Tuberculosis__0.07-ococo16-noremap/conf.py
@@ -10,12 +10,6 @@ conf_description+="mutrate {}; ".format(conf_mutrate)
 
 bwa = dymas.Mapping_BwaMem()
 
-consensus = dymas.Consensus_Ococo(
-		strategy="majority",
-		variant=32,
-		remapping=True,
-	)
-
 sorting = dymas.Sorting_FakeNoSort()
 
 pileup = dymas.Pileup_FakeEmpty()
