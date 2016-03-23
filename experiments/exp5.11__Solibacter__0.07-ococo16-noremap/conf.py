50186ce51cca825136e03be4701dc6327262ac5d
Karel Břinda <karel.brinda@gmail.com>
2016-03-23 23:31:32 +0100

    Add Exp5 Solibacter

---
+++ experiments/exp5.11__Solibacter__0.07-ococo16-noremap/conf.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, "../")
+from conf_exp5 import *
+
+conf_mutrate=0.07
+
+conf_description+="mutrate {}; ".format(conf_mutrate)
+
+##############################
+
+bwa = dymas.Mapping_BwaMem()
+
+sorting = dymas.Sorting_FakeNoSort()
+
+pileup = dymas.Pileup_FakeEmpty()
