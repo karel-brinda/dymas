69d2a98dd0329164c074d75af5e9302e0c2bafa9
Karel Břinda <karel.brinda@gmail.com>
2016-11-10 11:52:59 +0100

    Change exps to yeast

--- experiments/conf_exp1.py
+++ experiments/conf_exp1.py
@@ -1,10 +1,11 @@
 from conf_global import *
 
-conf_reference="../exp0.00__data/Borrelia_garinii.fa"
+conf_reference="../exp0.00__data/yeast.fa"
 conf_coverage=10
 
-conf_title+="Borrelia garinii hapl"
+conf_title+="Yeast (saccharomyces_cerevisiae)"
 #conf_description+="cov 5; "
 conf_allowed_delta=5
 
 haploid_mode=True
+
